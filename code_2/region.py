a6e93e4e60fa0abce0552403f984cef8ef3b913d
rodrigoms95 <rodrigoms95@gmail.com>
2024-11-13 13:32:29 -0600

    regions

--- code_2/region.py
+++ code_2/region.py
@@ -37,22 +37,14 @@ prod_n           = track_P_mp[0:5] + track_P_mp[10:]
 
 # Para generación distribuida ponderamos con el área construida
 prod_n_dist = [f"{x}_distributed" for x in prod_n]
-a = ds[["built_surface", "REGION"]].groupby("REGION").sum().to_dataframe()
-ds["sum_built_surface"] = ds["REGION"].to_dataframe().join(
-    a, on = "REGION")["built_surface"].to_xarray().astype(np.float32)
-ds["perc_built_surface"] = (ds["built_surface"] / ds["sum_built_surface"]
-    ).astype(np.float32)
-ds[prod_n_dist] = (ds[prod_n] * ds["perc_built_surface"]).astype(np.float32)
+ds[prod_n_dist] = (ds[prod_n] * ds["built_surface"]).astype(np.float32)
 # Podemos forzar a que la producción distribuida siempre sea hacia el sur
-for v in prod_n_dist[1:]: ds[v] = ds[prod_n_dist[0]]
+#for v in prod_n_dist[1:]: ds[v] = ds[prod_n_dist[0]]
+
 # Para parques solares ponderamos con las líneas de transmisión
 prod_n_centr = [f"{x}_central" for x in prod_n]
 ds[prod_n_centr] = ds[prod_n].where(ds["potential_solar_park_zones"]
     ).astype(np.float32)
-# Unimos toda la producción
-prod_n_total = [f"{x}_total" for x in prod_n]
-for i, v in enumerate(prod_n_total):
-    ds[v] = ((ds[prod_n_dist[i]] + ds[prod_n_centr[i]])/2).astype(np.float32)
 
 # Promediamos la generación por región
 ds_c = ds.groupby("REGION").mean()
@@ -70,6 +62,27 @@ ds_i["REGION"] = ds["REGION"].where(ds["REGION"] == 24, 24)
 ds_c_4 = ds_i.groupby("REGION").mean()
 ds_c = xr.concat([ds_c, ds_c_2, ds_c_3, ds_c_4], "REGION")
 ds_c["REGION"] = ds_c["REGION"].astype(int)
+
+# Para generación distribuida ponderamos con el área construida
+a = ds[["built_surface", "REGION"]].groupby("REGION").sum().to_dataframe()
+a.loc[22] = a[a["REGION"].isin([1, 2, 4, 5, 6, 14, 19])].sum()
+a.loc[23] = a[a["REGION"].isin([1, 2, 10, 11, 12, 13])].sum()
+a.loc[24] = a.sum()
+b = ds[["built_surface", "REGION"]].groupby("REGION").count().to_dataframe()
+b.loc[22] = a[a["REGION"].isin([1, 2, 4, 5, 6, 14, 19])].sum()
+b.loc[23] = a[a["REGION"].isin([1, 2, 10, 11, 12, 13])].sum()
+b.loc[24] = a.sum()
+ds["sum_built_surface"] = ds["REGION"].to_dataframe().join(
+    a, on = "REGION")["built_surface"].to_xarray().astype(np.float32)
+ds["count_built_surface"] = ds["REGION"].to_dataframe().join(
+    b, on = "REGION")["built_surface"].to_xarray().astype(np.float32)
+ds[prod_n_dist] = (ds[prod_n_dist] * ds["count_built_surface"]
+    / ds["sum_built_surface"])
+
+# Unimos toda la producción
+prod_n_total = [f"{x}_total" for x in prod_n]
+for i, v in enumerate(prod_n_total): ds_c[v] = (
+    (ds_c[prod_n_dist[i]] + ds_c[prod_n_centr[i]])/2).astype(np.float32)
 
 delayed_obj = ds_c.to_netcdf(f"{dir_r}full_disc_region_2022.nc",
     compute = False)
